947ef4d13073c48ec5d549aa403d6ab29d9411d0
kaan ceylan <114739772+kaanceylon@users.noreply.github.com>
2022-11-22 11:45:06 +0300

    Add files via upload

---
+++ Hermonic array.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+       int[] numbers = {1,2,3,4,5};
+       double sum = 0;
+
+       for (int i : numbers){
+           sum += (1.0/i);
+       }
+
+       double harmonicAverage = numbers.length / sum;
+        System.out.println("Harmonik Ortalama = " + harmonicAverage);
+
+    }
+}
